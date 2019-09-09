f02c4495b8919947fa36fd1215e5ce30fcfe54b9
khanhtran94 <khanhtrantung190894@gmail.com>
2019-09-10 00:50:19 +0700

    todo phase 2
    them css

--- src/App.js
+++ src/App.js
@@ -2,32 +2,13 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-
+import TodoItem from "./components/TodoItem";
 function App() {
-  const firstName = "joint";
-  const lastName = "bob";
-  const date = new Date();
-  const styles = {
-    fontSize: 40,
-    color: "#111111"
-  };
-
-  const hours = date.getHours();
-  let timeDate;
-
-  if (hours < 12) {
-    timeDate = "Morning";
-    styles.backgroundColor = "#ff2d00";
-  } else {
-    timeDate = "buoi chieu";
-    styles.backgroundColor = "#ffffcc";
-  }
   return (
     <div>
-      <h1>hello {firstName + " " + lastName}</h1>
-      <h2>hello {`${firstName} ${lastName}`}</h2>
-      <p style={{ color: "#ff8c00" }}>it is {date.getHours()}</p>
-      <p style={styles}>This is styles {`${timeDate}`}</p>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>vision 1</p>
+    </div>
+  );
+}
+export default TodoItem;
