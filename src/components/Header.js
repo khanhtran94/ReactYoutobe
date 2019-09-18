ae2f5e2223c04b5e42a086efa6d8f682a776f3e5
khanhtran94 <khanhtrantung190894@gmail.com>
2019-09-18 23:50:14 +0700

    class base component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-function Header() {
-  return (
-    <nav className="navbar">
-      <h1>Hello Khanh</h1>
-      <ul>
-        <li>thing 1</li>
-        <li>thing 2</li>
-      </ul>
-    </nav>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <h1>Hello {this.props.username}</h1>
+        <ul>
+          <li>thing 1</li>
+          <li>thing 2</li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Header;
